e8229a4ce6b61dc0d23ccc8666c0fd54805fffff
YUNUS KIRAN <yunus.kiran@turkcell.com.tr>
2020-06-14 18:34:17 +0300

    rx.extension sample console project added.

---
+++ c#/console/Rx.Extension/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rx.Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
