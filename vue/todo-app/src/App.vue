7d8a214c7647a2d5e3e6722cf31fd67bd6c6d64e
YUNUS KIRAN <yunus.kiran@turkcell.com.tr>
2021-11-11 01:19:52 +0300

    vue todo

---
+++ vue/todo-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <TodoForm :about="about" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import TodoForm from "./components/TodoForm.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    TodoForm,
+  },
+
+  setup() {
+    const about = ref({
+      title: "Vue 3",
+      subTitle: "TypeScript demo",
+    });
+
+    return {
+      about,
+    };
+  },
+});
+</script>
+
+<style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.css";
+</style>
---
+++ vue/todo-app/src/models/todoModel.ts
@@ -0,0 +1,5 @@
+export interface TodoModel {
+  id: string;
+  done: boolean;
+  content: string;
+}
